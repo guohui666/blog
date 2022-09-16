29f808c05cbe981302ccb0b3ffecc0f1886e4343
guohui <guohui@youxiuqn.com>
2022-09-16 16:03:32 +0800

    123

--- spring/spring.js
+++
@@ -1,5 +0,0 @@
-var link = document.createElement( 'link' );
-link.href = 'https://guohuihui.gitee.io/guohui-blog/spring/spring.css';
-link.rel ='stylesheet';
-link.type = 'text/css';
-document.head.appendChild(link);
