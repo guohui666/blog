d411e675fea193845821408d40ba14c0fdb32958
guohui <guohui@youxiuqn.com>
2022-09-22 17:55:59 +0800

    提交

--- static/spring/spring.js
+++ static/spring/spring.js
@@ -1,10 +1,9 @@
-
-window.onload = ()=>{
-    var link = document.createElement( 'link' );
-    link.href = 'https://guohuihui.gitee.io/guohui-blog/spring/spring.css';
-    link.rel ='stylesheet';
-    link.type = 'text/css';
-    document.head.appendChild(link);
+window.onload = () => {
+    var link = document.createElement('link')
+    link.href = 'https://guohuihui.gitee.io/guohui-blog/spring/spring.css'
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    document.head.appendChild(link)
     var qrcodeElement = document.createElement('div')
     var headerContainer = document.getElementsByClassName('not-mobile')[0]
 
@@ -13,4 +12,13 @@ window.onload = ()=>{
     <img src="https://guohuihui.gitee.io/guohui-blog/spring/qrcode-green.webp" alt="">
 `
     headerContainer.appendChild(qrcodeElement)
+
+
+    var qrcodeElementMobile = document.createElement('div')
+    var form = document.getElementsByClassName('form')[0]
+    var main = document.getElementById('main')
+
+    qrcodeElementMobile.setAttribute('class', 'spring-qrcode-mobile')
+    qrcodeElementMobile.innerHTML = `<img src="https://guohuihui.gitee.io/guohui-blog/spring/qrcode-green.webp" alt="">`
+    main.insertBefore(qrcodeElementMobile, form)
 }
