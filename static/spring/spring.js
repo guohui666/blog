3f7369503b3ae5f8186416234444299410bc6a98
guohui <guohui@youxiuqn.com>
2022-09-22 16:55:27 +0800

    提交

--- static/spring/spring.js
+++ static/spring/spring.js
@@ -3,3 +3,12 @@ link.href = 'https://guohuihui.gitee.io/guohui-blog/spring/spring.css';
 link.rel ='stylesheet';
 link.type = 'text/css';
 document.head.appendChild(link);
+
+var qrcodeElement = document.createElement('div')
+var headerContainer = document.getElementsByClassName('not-mobile')[0]
+
+qrcodeElement.setAttribute('class', 'spring-qrcode')
+qrcodeElement.innerHTML = `
+    <img src="https://guohuihui.gitee.io/guohui-blog/spring/qrcode-green.png" alt="">
+`
+headerContainer.appendChild(qrcodeElement)
